712116e5532ac9ae7f48d40c1756391eb2ae4b9c
kushan2k <kushangayantha001@gmail.com>
2023-06-16 17:18:44 +0530

    page designed

--- web/main.py
+++ web/main.py
@@ -1,19 +1,20 @@
 from flask import Flask,render_template
 from jinja2.exceptions import TemplateNotFound
 from dotenv import load_dotenv
+import joblib
 
 
 load_dotenv()
 
 app=Flask(__name__)
+model=joblib.load('../model.joblib')
 
 @app.route("/")
 def Index():
+#     feature names ['gender' 'age' 'hypertension' 'heart_disease' 'smoking_history' 'bmi'
+#  'HbA1c_level' 'blood_glucose_level']
+
     try:
-        return render_template("index.html")
+        return render_template("index.html",title="Home")
     except TemplateNotFound:
         return "template not found"
-
-
-if __name__=='__main__':
-    app.run()
